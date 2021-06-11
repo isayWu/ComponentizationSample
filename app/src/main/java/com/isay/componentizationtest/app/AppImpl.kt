582a77781a8fba492f3ed81d4bbfc238b9e2cda3
isayWu <727643332@qq.com>
2021-06-11 15:38:31 +0800

    Update AppImpl.kt

--- app/src/main/java/com/isay/componentizationtest/app/AppImpl.kt
+++ app/src/main/java/com/isay/componentizationtest/app/AppImpl.kt
@@ -7,15 +7,8 @@ import com.isay.commonlib.app.IApplicationDelegate
 /**
  * Desc:
  *
- *
- * Author: WuCongYi
  * Date: 2020/4/29
  * Copyright: Copyright (c) 2016-2020
- * Company: @小牛科技
- * Update Comments:
- * 构建配置参见:
- *
- * @author wucongyi
  */
 class AppImpl : IApplicationDelegate{
 
